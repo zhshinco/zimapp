d1e31246e64c1a5619bb4672adde7796e6ad6342
洪 周 <hongzhou@hongdeMacBook-Air.local>
2017-07-26 22:47:01 +0800

    init commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="vux-demo">
+      <img class="logo" src="../assets/vux_logo.png">
+      <h1> </h1>
+    </div>
+    <group title="cell demo">
+      <cell title="VUX" value="cool" is-link></cell>
+    </group>
+    <tabbar>
+      <tabbar-item selected link="/">
+        <img slot="icon" src="../assets/logo.png">
+        <span slot="label">Wechat</span>
+      </tabbar-item>
+      <tabbar-item show-dot link="/hello">
+        <img slot="icon" src="../assets/vux_logo.png">
+        <span slot="label">Message</span>
+      </tabbar-item>
+      <tabbar-item  >
+        <img slot="icon" src="../assets/logo.png">
+        <span slot="label">Explore</span>
+      </tabbar-item>
+      <tabbar-item badge="2">
+        <img slot="icon" src="../assets/vux_logo.png">
+        <span slot="label">News</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import { Tabbar, TabbarItem,Group, Cell } from 'vux'
+
+export default {
+  components: {
+    Tabbar,
+    TabbarItem,
+    Group,
+    Cell
+  },
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style>
+.vux-demo {
+  text-align: center;
+}
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <group title="cell demo">
+      <cell title="VUX" value="cool" is-link></cell>
+    </group>
+    <tabbar>
+      <tabbar-item selected link="/">
+        <img slot="icon" src="../assets/logo.png">
+        <span slot="label">Wechat</span>
+      </tabbar-item>
+      <tabbar-item show-dot link="/hello">
+        <img slot="icon" src="../assets/vux_logo.png">
+        <span slot="label">Message</span>
+      </tabbar-item>
+      <tabbar-item  >
+        <img slot="icon" src="../assets/logo.png">
+        <span slot="label">Explore</span>
+      </tabbar-item>
+      <tabbar-item badge="2">
+        <img slot="icon" src="../assets/vux_logo.png">
+        <span slot="label">News</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import { Tabbar, TabbarItem, Group, Cell } from 'vux'
+
+export default {
+  components: {
+    Tabbar,
+    TabbarItem,
+    Group,
+    Cell
+  }
+}
+</script>>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
